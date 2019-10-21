83428e52418ba9d8f6945b82491a923fe1f76baa
thomas maestas <thomasm1.maestas@gmail.com>
2019-10-20 18:59:10 -0600

    import rest of observables + stylization

--- public/rxjs-functions.js
+++ public/rxjs-functions.js
@@ -1,70 +1,60 @@
-// Syntax
-const observable = Rx.Observable.create(observer => {
-    observer.next('this is ...')
-    observer.next('...your data stream!\n\n')
-})
-observable.subscribe(val => print1('1. ' + val))
-    // mouseEvents
-const clicks = Rx.Observable.fromEvent(document, 'click')
-clicks.subscribe(click => console.log(click))
-clicks.subscribe(click => print2('X: ' + JSON.stringify(click.clientX) + ' | ' + 'Y: ' + JSON.stringify(click.clientY)))
-
-const promesa = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve("2. ...promise resolved\n\n");
-    }, 0);
-});
-const observaPromesa = Rx.Observable.fromPromise(promesa);
-observaPromesa.subscribe(result => print1(result));
-///
-const arrayDelay = [{
-    "Date": "2019-07-04",
-    "Symbol": "BTCUSD",
-    "Open": 11976.42,
-    "High": 12064.26,
-    "Low": 11820,
-    "Close": 11909.55,
-    "Volume BTC": 1237.57,
-    "Volume USD": 14790355.69
-},
-{
-    "Date": "2019-07-04",
-    "Symbol": "ETHUSD",
-    "Open": 301.79,
-    "High": 304,
-    "Low": 300.39,
-    "Close": 301.5,
-    "Volume ETH": 3825.69,
-    "Volume USD": 1154988.12
-}
-];
-const addedArrayDelay = 
-{
-    "Date": "2019-07-05",
-    "Symbol": "LITECOIN",
-    "Open": 101.79,
-    "High": 104,
-    "Low": 100.39,
-    "Close": 101.5,
-    "Volume LTC": 825.69,
-    "Volume USD": 7777777.77
-}
-function afterTheDelay() {
-setTimeout(() => {
-    let outputPromise = "";
-    arrayDelay.forEach((arrayDelay, index) => {
-        outputPromise += `<li>Date: ${arrayDelay.Date} Symbol:  ${arrayDelay.Symbol} Close: ${arrayDelay.Close}</li>`;
+(function() {
+    const promesa = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("2. ...promise resolved\n\n");
+        }, 0);
     });
-    document.getElementById("promises").innerHTML = "<strong>Local Ether + Bitcoin:</strong><br />" + outputPromise;
-}, 2000);
-}
+    const observaPromesa = Rx.Observable.fromPromise(promesa);
+    observaPromesa.subscribe(result => print1(result));
+    ///
+    const arrayDelay = [{
+            "Date": "2019-07-04",
+            "Symbol": "BTCUSD",
+            "Open": 11976.42,
+            "High": 12064.26,
+            "Low": 11820,
+            "Close": 11909.55,
+            "Volume BTC": 1237.57,
+            "Volume USD": 14790355.69
+        },
+        {
+            "Date": "2019-07-04",
+            "Symbol": "ETHUSD",
+            "Open": 301.79,
+            "High": 304,
+            "Low": 300.39,
+            "Close": 301.5,
+            "Volume ETH": 3825.69,
+            "Volume USD": 1154988.12
+        }
+    ];
+    const addedArrayDelay = {
+        "Date": "2019-07-05",
+        "Symbol": "LITECOIN",
+        "Open": 101.79,
+        "High": 104,
+        "Low": 100.39,
+        "Close": 101.5,
+        "Volume LTC": 825.69,
+        "Volume USD": 7777777.77
+    }
+
+    function afterTheDelay() {
+        setTimeout(() => {
+            let outputPromise = "";
+            arrayDelay.forEach((arrayDelay, index) => {
+                outputPromise += `<li>Date: ${arrayDelay.Date} Symbol:  ${arrayDelay.Symbol} Close: ${arrayDelay.Close}</li>`;
+            });
+            document.getElementById("promises").innerHTML = "<strong>Local Ether + Bitcoin:</strong><br />" + outputPromise;
+        }, 2000);
+    }
 
     function addPromise(price) {
-        return new Promise((resolve, reject) => { 
+        return new Promise((resolve, reject) => {
             setTimeout(() => {
                 arrayDelay.push(price);
                 console.log("hey num2")
-                // const error = true;
+                    // const error = true;
                 const error = false;
                 if (!error) {
                     resolve();
@@ -74,11 +64,10 @@ setTimeout(() => {
             }, 2000);
         });
     }
-        
+
     const promesa_New = new Promise((resolve, reject) => {
-        setInterval(() => {  
-            resolve(arrayDelay.push(
-                {                   
+        setInterval(() => {
+            resolve(arrayDelay.push({
                 "Date": "2019-07-05",
                 "Symbol": "NEWCOIN",
                 "Open": 101.79,
@@ -87,10 +76,9 @@ setTimeout(() => {
                 "Close": 101.5,
                 "Volume LTC": 825.69,
                 "Volume USD": 7777777.77
-            }
-            ));  
-           arrayDelay.push(addedArrayDelay) 
-           console.log("inside Promes_NEW")
+            }));
+            arrayDelay.push(addedArrayDelay)
+            console.log("inside Promes_NEW")
         }, 3000);
     });
     ///
@@ -98,48 +86,50 @@ setTimeout(() => {
 
     const observaPromesa_New = Rx.Observable.fromPromise(promesa_New);
     observaPromesa_New.subscribe(result => print3(result));
-    
+
     async function initAsync() {
-    await addPromise({
-        "Date": "2020-01-02",
-        "Symbol": "coinASYNCAdded",
-        "Open": 13000.00,
-        "High": 14000,
-        "Low": 12000.00,
-        "Close": 1350.00
-    });
-    afterTheDelay();
+        await addPromise({
+            "Date": "2020-01-02",
+            "Symbol": "coinASYNCAdded",
+            "Open": 13000.00,
+            "High": 14000,
+            "Low": 12000.00,
+            "Close": 1350.00
+        });
+        afterTheDelay();
     }
-    initAsync(); 
-/// 
+    initAsync();
+    /// 
 
 
 
 
-const timer = Rx.Observable.timer(0)
-timer.subscribe(done => print1('3. timer ding!\n\n'))
-// const interval = Rx.Observable.interval(1000)
-// interval.subscribe(int => print1(new Date().getSeconds())) 
-const mashup = Rx.Observable.of('string', ['array'], 12) //, true, { property: 'value' })
-mashup.subscribe(val => print1('4. ' + val + '\n\n'))
+    const timer = Rx.Observable.timer(0)
+    timer.subscribe(done => print1('3. timer ding!\n\n'))
+        // const interval = Rx.Observable.interval(1000)
+        // interval.subscribe(int => print1(new Date().getSeconds())) 
+    const mashup = Rx.Observable.of('string', ['array'], 12) //, true, { property: 'value' })
+    mashup.subscribe(val => print1('4. ' + val + '\n\n'))
 
-/////////////////////////////////
-// PRINT FUNCTIONS
-/////////////////////////////////
-// left colum
-function print1(val) {
-    let el = document.createElement('p')
-    el.innerText = val;
-    document.getElementById("analytics").appendChild(el)
-}
-function print3(val) {
-    let el2 = document.createElement('p');
-    el2.innerText = val
-    document.getElementById("analyticsNum2").appendChild(el2)
-}
-// right column
-function print2(val) {
-    let el = document.createElement('p')
-    el.innerText = val;
-    document.getElementById("analytics2").appendChild(el)
-}
+    /////////////////////////////////
+    // PRINT FUNCTIONS
+    /////////////////////////////////
+    // left colum
+    function print1(val) {
+        let el = document.createElement('p')
+        el.innerText = val;
+        document.getElementById("analytics").appendChild(el)
+    }
+
+    function print3(val) {
+        let el2 = document.createElement('p');
+        el2.innerText = val
+        document.getElementById("analyticsNum2").appendChild(el2)
+    }
+    // right column
+    function print2(val) {
+        let el = document.createElement('p')
+        el.innerText = val;
+        document.getElementById("analytics2").appendChild(el)
+    }
+})();
